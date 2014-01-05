755a6a69e6bbd9af1a6152903c04b9533adf7748
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-01-05 21:05:13 +0400

    PersistApiReferences draft

--- plugin.groovy
+++ plugin.groovy
@@ -1,8 +1,11 @@
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiWhiteSpace
+import com.intellij.psi.*
+import com.intellij.util.containers.StripedLockIntObjectConcurrentHashMap
+import org.jetbrains.annotations.NotNull
+import org.jetbrains.annotations.Nullable
 import org.neo4j.graphdb.DynamicLabel
 import org.neo4j.graphdb.DynamicRelationshipType
 import org.neo4j.graphdb.RelationshipType
@@ -13,7 +16,6 @@ import org.neo4j.unsafe.batchinsert.BatchInserters
 import static liveplugin.PluginUtil.*
 import static org.neo4j.helpers.collection.MapUtil.map
 import static org.neo4j.helpers.collection.MapUtil.stringMap
-
 //
 // Sample neo4j cypher queries:
 // match (class:PsiClassImpl) return count(class)
@@ -24,23 +26,86 @@ import static org.neo4j.helpers.collection.MapUtil.stringMap
 
 // add-to-classpath $HOME/IdeaProjects/neo4j-tutorial/lib/*.jar
 
+
 doInBackground("Copying PSI to neo4j") { indicator ->
 	runReadAction {
 		using(inserter(pluginPath + "/neo-database")) { BatchInserter inserter ->
-			new PersistPsi(inserter).persist(project)
+			new PersistPsiHierarchy(inserter, neo4jKey()).persist(project)
+//			new PersistApiReferences(inserter, neo4jKey()).persist(project)
 		}
 	}
 	show("Finished copying PSI")
 }
 
-class PersistPsi {
+
+class PersistApiReferences {
+	private final RelationshipType refersTo = DynamicRelationshipType.withName("REFERS_TO")
+
+	private final BatchInserter inserter
+	private final Key<Long> neo4jKey
+	private Project project
+
+	PersistApiReferences(BatchInserter inserter, Key<Long> neo4jKey) {
+		this.inserter = inserter
+		this.neo4jKey = neo4jKey
+	}
+
+	def persist(Project project) {
+		this.project = project
+
+		def roots = ProjectRootManager.getInstance(project).contentSourceRoots
+		roots.each { VirtualFile sourceRoot ->
+			persistSourceRoot(sourceRoot)
+		}
+	}
+
+	private persistSourceRoot(VirtualFile sourceRoot) {
+		sourceRoot.each { child ->
+			persistFile(child)
+		}
+	}
+
+	private persistFile(VirtualFile virtualFile) {
+		def psiFile = psiFile(virtualFile, project)
+		if (psiFile == null) {
+			virtualFile.children.each{
+				persistFile(it)
+			}
+		} else {
+			persistPsiElement(psiFile)
+		}
+	}
+
+	private persistPsiElement(PsiElement element) {
+		if (element instanceof PsiReference) {
+			def resolvedElement = element.resolve()
+			if (resolvedElement == null) return
+
+			def elementId = element.getUserData(neo4jKey)
+			def resolvedElementId = resolvedElement.getUserData(neo4jKey)
+
+			if (elementId == null) log("No neo4j key for element: " + element)
+			if (resolvedElementId == null) log("No neo4j key for resolved: " + resolvedElement)
+
+			inserter.createRelationship(elementId, resolvedElementId, refersTo, map())
+		}
+
+		element.children.findAll{ !(it instanceof PsiWhiteSpace) }.each{
+			persistPsiElement(it)
+		}
+	}
+}
+
+class PersistPsiHierarchy {
 	private final RelationshipType childOf = DynamicRelationshipType.withName("CHILD_OF")
 
 	private final BatchInserter inserter
+	private final Key<Long> neo4jKey
 	private Project project
 
-	PersistPsi(BatchInserter inserter) {
+	PersistPsiHierarchy(BatchInserter inserter, Key<Long> neo4jKey) {
 		this.inserter = inserter
+		this.neo4jKey = neo4jKey
 	}
 
 	def persist(Project project) {
@@ -72,7 +137,6 @@ class PersistPsi {
 			virtualFile.children.each{
 				persistFile(it, node)
 			}
-
 		} else {
 			persistPsiElement(psiFile, parentNode)
 		}
@@ -82,12 +146,39 @@ class PersistPsi {
 		long node = inserter.createNode(map("string", element.toString()), DynamicLabel.label(element.class.simpleName))
 		inserter.createRelationship(node, parentNodeId, childOf, map())
 
+		element.putUserData(neo4jKey, node)
+
 		element.children.findAll{!(it instanceof PsiWhiteSpace)}.each {
 			persistPsiElement(it, node)
 		}
 	}
+
+	@Nullable private static PsiFileSystemItem psiItem(@Nullable VirtualFile file, @NotNull Project project) {
+		if (file == null) return null
+
+		def psiFile = PsiManager.getInstance(project).findFile(file)
+		if (psiFile != null) return psiFile
+
+		PsiManager.getInstance(project).findDirectory(file)
+	}
+
 }
 
+
+
+static Key<Long> neo4jKey() {
+	Key<Long> key = findKeyByName("Neo4jId")
+	if (key == null) key = Key.create("Neo4jId")
+	key
+}
+
+static <T> Key<T> findKeyByName(String name) {
+	for (StripedLockIntObjectConcurrentHashMap.IntEntry<Key> entry : Key.allKeys.entries()) {
+		if (name == entry.getValue().toString())  // assume that toString() returns name
+			return entry.getValue()
+	}
+	null
+}
 
 static using(BatchInserter inserter, Closure closure) {
 	try {
