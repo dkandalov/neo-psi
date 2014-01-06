2ad9bf7b9974e85cb1f4801f47348cb83e048c90
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-01-07 02:10:06 +0400

    resolve reference outside of project; extracted Neo4jKey class; use indicator.canceled in traverser

--- plugin.groovy
+++ plugin.groovy
@@ -13,11 +13,11 @@ import org.neo4j.kernel.DefaultFileSystemAbstraction
 import org.neo4j.unsafe.batchinsert.BatchInserter
 import org.neo4j.unsafe.batchinsert.BatchInserters
 
+import static java.util.concurrent.Executors.newSingleThreadExecutor
 import static liveplugin.PluginUtil.*
 import static org.neo4j.graphdb.DynamicLabel.label
 import static org.neo4j.helpers.collection.MapUtil.map
 import static org.neo4j.helpers.collection.MapUtil.stringMap
-
 //
 // Sample neo4j cypher queries:
 // match (class:PsiClassImpl) return count(class)
@@ -28,23 +28,33 @@ import static org.neo4j.helpers.collection.MapUtil.stringMap
 // match (file) where file.string =~ 'PsiDirectory.*extensions' return file
 //
 
+// Expected results for JUnit:
+// - 335139: match (n) return count(n)
+// - 334487: match ()<-[r:CHILD_OF]-() return count(r)
+// - 39225: match ()<-[r:REFERS_TO]-() return count(r)
+
 // add-to-classpath $HOME/IdeaProjects/neo4j-tutorial/lib/*.jar
 
 
+// TODO make this task modal
 doInBackground("Copying PSI to neo4j") { indicator ->
-	runReadAction {
-		using(inserter(pluginPath + "/neo-database")) { BatchInserter inserter ->
-			def key = neo4jKey()
-			def traversal = new ProjectTraversal(psiFilter())
-			traversal.traverse(project, persistPsiHierarchy(inserter, key))
-			traversal.traverse(project, persistPsiReferences(inserter, key))
+	newSingleThreadExecutor().submit({
+		runReadAction{
+			catchingAll{
+				using(inserter(pluginPath + "/neo-database")){ BatchInserter inserter ->
+					def key = new Neo4jKey()
+					def traversal = new ProjectTraversal(indicator, psiFilter())
+					traversal.traverse(project, persistPsiHierarchy(inserter, key))
+					traversal.traverse(project, persistPsiReferences(inserter, key))
+				}
+			}
 		}
-	}
+	}).get()
 	show("Finished copying PSI")
 }
 
 
-static Callback persistPsiReferences(BatchInserter inserter, Key<Long> neo4jKey) {
+static Callback persistPsiReferences(BatchInserter inserter, Neo4jKey neo4jKey) {
 	RelationshipType refersTo = DynamicRelationshipType.withName("REFERS_TO")
 
 	new Callback() {
@@ -53,8 +63,13 @@ static Callback persistPsiReferences(BatchInserter inserter, Key<Long> neo4jKey)
 			def resolvedElement = psiElement.asType(PsiReference).resolve()
 			if (resolvedElement == null) return
 
-			def elementId = neo4jKey.get(psiElement)
-			def resolvedElementId = neo4jKey.get(resolvedElement)
+			def elementId = neo4jKey.getId(psiElement)
+			def resolvedElementId = neo4jKey.getId(resolvedElement)
+
+			if (resolvedElementId == null) {
+				resolvedElementId = inserter.createNode(map("string", resolvedElement.toString()), label(resolvedElement.class.simpleName))
+				neo4jKey.setId(resolvedElement, resolvedElementId)
+			}
 
 			if (elementId != null && resolvedElementId != null) {
 
@@ -62,7 +77,6 @@ static Callback persistPsiReferences(BatchInserter inserter, Key<Long> neo4jKey)
 
 			} else {
 				// TODO No neo4j key for element: PsiJavaCodeReferenceElement:org.junit.runner.notification in /Users/dima/IdeaProjects/junit/src/main/java/org/junit/runner/notification/package-info.java
-				// TODO track dependencies outside of project "if (resolvedElementId == null)"
 				if (elementId == null) {
 					def rootParent = psiElement.parent
 					while (rootParent != null && !(rootParent instanceof PsiFile)) rootParent = rootParent.parent
@@ -74,35 +88,63 @@ static Callback persistPsiReferences(BatchInserter inserter, Key<Long> neo4jKey)
 }
 
 
-static Callback persistPsiHierarchy(BatchInserter inserter, Key<Long> neo4jKey) {
+static Callback persistPsiHierarchy(BatchInserter inserter, Neo4jKey neo4jKey) {
 	RelationshipType childOf = DynamicRelationshipType.withName("CHILD_OF")
 
 	new Callback() {
 		@Override def onProject(Project aProject) {
 			long nodeId = inserter.createNode(map("string", aProject.name), label("Project"))
-			neo4jKey.set(aProject, nodeId)
+			neo4jKey.setId(aProject, nodeId)
 		}
 
 		@Override def onSourceRoot(VirtualFile sourceRoot, UserDataHolder parent) {
 			long nodeId = inserter.createNode(map("string", sourceRoot.path), label("SourceRoot"), label(sourceRoot.name))
-			inserter.createRelationship(nodeId, neo4jKey.get(parent), childOf, map())
-			neo4jKey.set(sourceRoot, nodeId)
+			inserter.createRelationship(nodeId, neo4jKey.getId(parent), childOf, map())
+			neo4jKey.setId(sourceRoot, nodeId)
 		}
 
 		@Override def onVirtualFile(VirtualFile virtualFile, UserDataHolder parent) {
 			long nodeId = inserter.createNode(map("string", virtualFile.name), label(virtualFile.name))
-			inserter.createRelationship(nodeId, neo4jKey.get(parent), childOf, map())
-			neo4jKey.set(virtualFile, nodeId)
+			inserter.createRelationship(nodeId, neo4jKey.getId(parent), childOf, map())
+			neo4jKey.setId(virtualFile, nodeId)
 		}
 
 		@Override def onPsiElement(PsiElement psiElement, UserDataHolder parent) {
+			if (psiElement instanceof PsiReference) psiElement.resolve()
+
 			long nodeId = inserter.createNode(map("string", psiElement.toString()), label(psiElement.class.simpleName))
-			inserter.createRelationship(nodeId, neo4jKey.get(parent), childOf, map())
-			neo4jKey.set(psiElement, nodeId)
+			inserter.createRelationship(nodeId, neo4jKey.getId(parent), childOf, map())
+			neo4jKey.setId(psiElement, nodeId)
 		}
 	}
 }
 
+class Neo4jKey {
+	private final Key<Long> neo4jKey = neo4jKey()
+
+	Long getId(UserDataHolder dataHolder) {
+		dataHolder.getUserData(neo4jKey)
+	}
+
+	def setId(UserDataHolder dataHolder, long id) {
+		dataHolder.putUserData(neo4jKey, id)
+	}
+
+	private static Key<Long> neo4jKey() {
+		Key<Long> key = findKeyByName("Neo4jId")
+		if (key == null) key = Key.create("Neo4jId")
+		key
+	}
+
+	@SuppressWarnings("GroovyAccessibility")
+	private static <T> Key<T> findKeyByName(String name) {
+		for (StripedLockIntObjectConcurrentHashMap.IntEntry<Key> entry : Key.allKeys.entries()) {
+			if (name == entry.getValue().toString())  // assume that toString() returns name
+				return entry.getValue()
+		}
+		null
+	}
+}
 
 class Callback {
 	def onProject(Project project) {}
@@ -113,10 +155,12 @@ class Callback {
 
 class ProjectTraversal {
 	private final Closure acceptPsi
+	private final def indicator
 	private Project project
 	private Callback callback
 
-	ProjectTraversal(Closure acceptPsi = null) {
+	ProjectTraversal(indicator, Closure acceptPsi = null) {
+		this.indicator = indicator
 		this.acceptPsi = acceptPsi
 	}
 
@@ -131,11 +175,15 @@ class ProjectTraversal {
 	}
 
 	private persistSourceRoot(VirtualFile sourceRoot, UserDataHolder parent) {
+		if (indicator.canceled) return
+
 		callback.onSourceRoot(sourceRoot, parent)
 		sourceRoot.children.each{ persistFile(it, sourceRoot) }
 	}
 
 	private persistFile(VirtualFile virtualFile, UserDataHolder parent) {
+		if (indicator.canceled) return
+
 		def psiItem = psiItem(virtualFile, project)
 		if (psiItem == null) {
 			callback.onVirtualFile(virtualFile, parent)
@@ -146,6 +194,7 @@ class ProjectTraversal {
 	}
 
 	private persistPsiElement(PsiElement element, UserDataHolder parent) {
+		if (indicator.canceled) return
 		if (acceptPsi != null && !acceptPsi(element)) return
 
 		callback.onPsiElement(element, parent)
@@ -164,21 +213,6 @@ class ProjectTraversal {
 	}
 }
 
-
-
-static Key<Long> neo4jKey() {
-	Key<Long> key = findKeyByName("Neo4jId")
-	if (key == null) key = Key.create("Neo4jId")
-	key
-}
-
-static <T> Key<T> findKeyByName(String name) {
-	for (StripedLockIntObjectConcurrentHashMap.IntEntry<Key> entry : Key.allKeys.entries()) {
-		if (name == entry.getValue().toString())  // assume that toString() returns name
-			return entry.getValue()
-	}
-	null
-}
 
 static using(BatchInserter inserter, Closure closure) {
 	try {
